41c7a96e898c3506c572f2d4457614fc9a9af855
LuanLB99 <luanlealboni@gmail.com>
2023-09-27 09:30:45 -0300

    Teste Variável de ambiente

--- src/connection.js
+++ src/connection.js
@@ -4,9 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 let db;
-const mongoClient = new MongoClient(
-  "mongodb+srv://user:user123@mywallet.rkxayzw.mongodb.net/?retryWrites=true&w=majority"
-);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 await mongoClient.connect().then(() => {
   db = mongoClient.db("mywallet");
 });
